20d24accaa7cd181410ecfaba9d3e62d59dad8d0
Anan Sadiya <anan.sadiya@e-campus.uab.cat>
2019-03-21 12:23:09 +0100

    right home improved!

--- FootNet/FootNet/Controller/RightHome/RightHomeViewController.swift
+++ FootNet/FootNet/Controller/RightHome/RightHomeViewController.swift
@@ -13,4 +13,29 @@ class RightHomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.colorDarkGreen
     }
+    
+    @IBAction func linkedInButton(_ sender: Any) {
+        let webURL = URL(string: "https://www.linkedin.com/in/anan-sadiya-26071994/")!
+        let appURL = URL(string: "linkedin://profile/anan-sadiya-26071994/")!
+        
+        if UIApplication.shared.canOpenURL(appURL) {
+            UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.open(webURL, options: [:], completionHandler: nil)
+        }
+    }
+    
+    @IBAction func instagramButton(_ sender: Any) {
+        let Username =  "Anansadiya94" // Your Instagram Username here
+        let appURL = URL(string: "instagram://user?username=\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if Instagram app is not installed, open URL inside Safari
+            let webURL = URL(string: "https://instagram.com/\(Username)")!
+            application.open(webURL)
+        }
+    }
 }

ebe2cca2107d0d83116f0d220cb10a923e4d5b99
Anan Sadiya <anan.sadiya@e-campus.uab.cat>
2019-03-21 16:53:49 +0100

    tab bar bug fixed!

--- FootNet/FootNet/Controller/MainTabBar/MainTabBarController.swift
+++ FootNet/FootNet/Controller/MainTabBar/MainTabBarController.swift
@@ -9,29 +9,35 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-    @IBOutlet weak var homeTabBarItem: UITabBar!
-    @IBOutlet weak var offerTabBarItem: UITabBar!
-    @IBOutlet weak var offersStatusTabBar: UITabBar!
-    @IBOutlet weak var searchTabBarItem: UITabBar!
-    @IBOutlet weak var notificationsTabBarItem: UITabBar!
-    
+    @IBOutlet weak var homeTabBar: UITabBar!
+    @IBOutlet weak var offerTabBar: UITabBar!
+    @IBOutlet weak var offersStatusTab: UITabBar!
+    @IBOutlet weak var searchTabBar: UITabBar!
+    @IBOutlet weak var notificationsTabBar: UITabBar!
+
+    let home = HomeViewController()
+    let offers = OffersViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Load all view
+        for viewController in self.viewControllers! {
+            _ = viewController.view
+        }
         setTabBar()
     }
     
     private func setTabBar() {
         tabBar.barTintColor = UIColor.black
-        homeTabBarItem.tintColor = UIColor.colorDarkGreen
-        homeTabBarItem.unselectedItemTintColor = UIColor.white
-        offerTabBarItem.tintColor = UIColor.colorDarkGreen
-        offerTabBarItem.unselectedItemTintColor = UIColor.white
-        offersStatusTabBar.tintColor = UIColor.colorDarkGreen
-        offersStatusTabBar.unselectedItemTintColor = UIColor.white
-        searchTabBarItem.tintColor = UIColor.colorDarkGreen
-        searchTabBarItem.unselectedItemTintColor = UIColor.white
-        notificationsTabBarItem.tintColor = UIColor.colorDarkGreen
-        notificationsTabBarItem.unselectedItemTintColor = UIColor.white
+        homeTabBar.tintColor = UIColor.colorDarkGreen
+        homeTabBar.unselectedItemTintColor = UIColor.white
+        offerTabBar.tintColor = UIColor.colorDarkGreen
+        offerTabBar.unselectedItemTintColor = UIColor.white
+        offersStatusTab.tintColor = UIColor.colorDarkGreen
+        offersStatusTab.unselectedItemTintColor = UIColor.white
+        searchTabBar.tintColor = UIColor.colorDarkGreen
+        searchTabBar.unselectedItemTintColor = UIColor.white
+        notificationsTabBar.tintColor = UIColor.colorDarkGreen
+        notificationsTabBar.unselectedItemTintColor = UIColor.white
     }
 }
 

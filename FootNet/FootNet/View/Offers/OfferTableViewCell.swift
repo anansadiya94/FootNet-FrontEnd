7d1e6fc1055eb605a8a1c7265aa38f69880a7dfc
Anan Sadiya <anan.sadiya@e-campus.uab.cat>
2019-04-30 11:15:24 +0200

    offer cell added

---
+++ FootNet/FootNet/View/Offers/OfferTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OfferTableViewCell.swift
+//  FootNet
+//
+//  Created by Anan Sadiya on 30/04/2019.
+//  Copyright © 2019 FootNet. All rights reserved.
+//
+
+import UIKit
+
+class OfferTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
